dcde3750d561a18b1c017b04fa591d72d44dc45d
Josh Bonfield <josh@grizzlyware.com>
2023-01-30 10:21:49 +0000

    feat: tie the outlet logic to the outlets themselves

--- arduino/arduino.go
+++ arduino/arduino.go
@@ -25,27 +25,19 @@ func (a Arduino) SetAllWaterState(state bool) error {
 	}
 }
 
-func (a Arduino) SetWaterState(outlet int, state bool) error {
+func (a Arduino) SetWaterState(outlet WaterOutlet, state bool) error {
+
+	var err error
 	var command Command
 
-	waterStateCommand := func(state bool, on Command, off Command) Command {
-		if state {
-			return on
-		} else {
-			return off
-		}
+	if state {
+		command, err = outlet.OnCommand()
+	} else {
+		command, err = outlet.OffCommand()
 	}
 
-	if outlet == 1 {
-		command = waterStateCommand(state, WATER_1_ON, WATER_1_OFF)
-	} else if outlet == 2 {
-		command = waterStateCommand(state, WATER_2_ON, WATER_2_OFF)
-	} else if outlet == 3 {
-		command = waterStateCommand(state, WATER_3_ON, WATER_3_OFF)
-	} else if outlet == 4 {
-		command = waterStateCommand(state, WATER_4_ON, WATER_4_OFF)
-	} else {
-		return errors.New("Invalid outlet")
+	if nil != err {
+		return err
 	}
 
 	return a.SendCommand(command)
