238975958e67d3c6f77a02decb46550f5f23ae8c
Josh Bonfield <josh@grizzlyware.com>
2023-01-31 10:51:06 +0000

    wip: export

--- helpers/slices.go
+++ helpers/slices.go
@@ -1,6 +1,6 @@
 package helpers
 
-func reverseSlice[S ~[]E, E any](s S) {
+func ReverseSlice[S ~[]E, E any](s S) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
