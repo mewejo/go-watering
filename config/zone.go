8d23648153d6d519f44d46280e347b02914455ce
Josh Bonfield <josh@grizzlyware.com>
2023-01-31 17:16:10 +0000

    debug: is it working?

--- config/zone.go
+++ config/zone.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"time"
 
@@ -25,6 +26,12 @@ func (z *Zone) SetWaterState(ard arduino.Arduino, state bool) {
 
 	if z.Watering == state {
 		return
+	}
+
+	if state {
+		fmt.Println("Starting water for zone " + z.Name)
+	} else {
+		fmt.Println("Stopping water for zone " + z.Name)
 	}
 
 	z.Watering = state
